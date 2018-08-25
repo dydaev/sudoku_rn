67c1930d1e7c5c653af278341e38f978dc9df53c
dydaev <dydaev@gmail.com>
2018-08-25 16:57:26 +0300

    added Utils, began board generator

--- App.js
+++ App.js
@@ -3,107 +3,147 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 
 import Call from './components/Call';
 import Block from './components/Block';
+import utils from './utils';
 
 export default class App extends React.Component {
   state = {
+    config: {
+        score: 4,
+
+        scoreType: {
+            3: [2, 3, 4],
+            4: [3, 4, 5],
+            5: [4, 5, 6],
+            6: [5, 6, 7],
+            7: [6, 7, 8],
+        }
+    },
     board: [
     [
-    	{ value: 1, access: 2},
-    	{ value: 2, access: 0},
-    	{ value: 3, access: 1},
-    	{ value: 4, access: 0},
-    	{ value: 5, access: 0},
-    	{ value: 6, access: 0},
-    	{ value: 7, access: 0},
-    	{ value: 8, access: 0},
-    	{ value: 9, access: 2},
+    { value: 1, access: 2},
+    { value: 2, access: 0},
+    { value: 3, access: 1},
+    { value: 4, access: 0},
+    { value: 5, access: 0},
+    { value: 6, access: 0},
+    { value: 7, access: 0},
+    { value: 8, access: 0},
+    { value: 9, access: 2},
     ],[
-    	{ value: 6, access: 2},
-    	{ value: 7, access: 0},
-    	{ value: 5, access: 0},
-    	{ value: 4, access: 0},
-    	{ value: 3, access: 1},
-    	{ value: 9, access: 0},
-    	{ value: 2, access: 1},
-    	{ value: 8, access: 0},
-    	{ value: 1, access: 2},
+    { value: 6, access: 2},
+    { value: 7, access: 0},
+    { value: 5, access: 0},
+    { value: 4, access: 0},
+    { value: 3, access: 1},
+    { value: 9, access: 0},
+    { value: 2, access: 1},
+    { value: 8, access: 0},
+    { value: 1, access: 2},
     ],[
-    	{ value: 3, access: 0},
-    	{ value: 2, access: 0},
-    	{ value: 4, access: 2},
-    	{ value: 1, access: 0},
-    	{ value: 5, access: 1},
-    	{ value: 9, access: 1},
-    	{ value: 7, access: 1},
-    	{ value: 8, access: 0},
-    	{ value: 6, access: 0},
+    { value: 3, access: 0},
+    { value: 2, access: 0},
+    { value: 4, access: 2},
+    { value: 1, access: 0},
+    { value: 5, access: 1},
+    { value: 9, access: 1},
+    { value: 7, access: 1},
+    { value: 8, access: 0},
+    { value: 6, access: 0},
     ],[
-    	{ value: 1, access: 2},
-    	{ value: 2, access: 0},
-    	{ value: 3, access: 1},
-    	{ value: 4, access: 0},
-    	{ value: 5, access: 0},
-    	{ value: 6, access: 0},
-    	{ value: 7, access: 0},
-    	{ value: 8, access: 0},
-    	{ value: 9, access: 2},
+    { value: 1, access: 2},
+    { value: 2, access: 0},
+    { value: 3, access: 1},
+    { value: 4, access: 0},
+    { value: 5, access: 0},
+    { value: 6, access: 0},
+    { value: 7, access: 0},
+    { value: 8, access: 0},
+    { value: 9, access: 2},
     ],[
-    	{ value: 6, access: 2},
-    	{ value: 7, access: 0},
-    	{ value: 5, access: 0},
-    	{ value: 4, access: 0},
-    	{ value: 3, access: 1},
-    	{ value: 9, access: 0},
-    	{ value: 2, access: 1},
-    	{ value: 8, access: 0},
-    	{ value: 1, access: 2},
+    { value: 6, access: 2},
+    { value: 7, access: 0},
+    { value: 5, access: 0},
+    { value: 4, access: 0},
+    { value: 3, access: 1},
+    { value: 9, access: 0},
+    { value: 2, access: 1},
+    { value: 8, access: 0},
+    { value: 1, access: 2},
     ],[
-    	{ value: 3, access: 0},
-    	{ value: 2, access: 0},
-    	{ value: 4, access: 2},
-    	{ value: 1, access: 0},
-    	{ value: 5, access: 1},
-    	{ value: 9, access: 1},
-    	{ value: 7, access: 1},
-    	{ value: 8, access: 0},
-    	{ value: 6, access: 1},
+    { value: 3, access: 0},
+    { value: 2, access: 0},
+    { value: 4, access: 2},
+    { value: 1, access: 0},
+    { value: 5, access: 1},
+    { value: 9, access: 1},
+    { value: 7, access: 1},
+    { value: 8, access: 0},
+    { value: 6, access: 1},
     ],[
-        { value: 1, access: 2},
-        { value: 2, access: 0},
-        { value: 3, access: 1},
-        { value: 4, access: 0},
-        { value: 5, access: 0},
-        { value: 6, access: 0},
-        { value: 7, access: 0},
-        { value: 8, access: 0},
-        { value: 9, access: 2},
+    { value: 1, access: 2},
+    { value: 2, access: 0},
+    { value: 3, access: 1},
+    { value: 4, access: 0},
+    { value: 5, access: 0},
+    { value: 6, access: 0},
+    { value: 7, access: 0},
+    { value: 8, access: 0},
+    { value: 9, access: 2},
     ],[
-        { value: 6, access: 2},
-        { value: 7, access: 0},
-        { value: 5, access: 0},
-        { value: 4, access: 0},
-        { value: 3, access: 1},
-        { value: 9, access: 0},
-        { value: 2, access: 1},
-        { value: 8, access: 0},
-        { value: 1, access: 2},
+    { value: 6, access: 2},
+    { value: 7, access: 0},
+    { value: 5, access: 0},
+    { value: 4, access: 0},
+    { value: 3, access: 1},
+    { value: 9, access: 0},
+    { value: 2, access: 1},
+    { value: 8, access: 0},
+    { value: 1, access: 2},
     ],[
-        { value: 3, access: 0},
-        { value: 2, access: 0},
-        { value: 4, access: 2},
-        { value: 1, access: 0},
-        { value: 5, access: 1},
-        { value: 9, access: 1},
-        { value: 7, access: 1},
-        { value: 8, access: 0},
-        { value: 6, access: 0},
+    { value: 3, access: 0},
+    { value: 2, access: 0},
+    { value: 4, access: 2},
+    { value: 1, access: 0},
+    { value: 5, access: 1},
+    { value: 9, access: 1},
+    { value: 7, access: 1},
+    { value: 8, access: 0},
+    { value: 6, access: 0},
     ],]
-  }
+}
 
-	handleChangeCall = (block, call, value) => {
-		const board = this.state.board;
-		board[block][call].value = value;
-		board[block][call].access = 2;
+handleGenerateBoard = () => {
+    let newBoard = new Array(9).reduce((board, _, indexBlock) => {
+        //generate count visible calls
+        const countVisibleCalls = utils.generateNumberBetween(this.state.config.score - 1, this.state.config.score + 1);
+        let arrVisibleCalls = [];
+
+        do {
+            //generate index fo visible calls
+            let numberCall = utils.generateNumberBetween(0, 8);
+            if (!arrVisibleCalls.includes(numberCall)) {
+                arrVisibleCalls.push(numberCall);
+                --countVisibleCalls;
+            }
+        } while (countVisibleCalls)
+
+        //generate new block
+        return [ ...board, new Array(9).map((_, indexCall) => {
+            //generate number for calls
+            return { value: indexCall, access: arrVisibleCalls.includes(indexCall) ? 2 : 0}
+        })];
+    },[]);
+
+
+    this.setState({
+        board: newBoard
+    });
+}
+
+handleChangeCall = (block, call, value) => {
+  const board = this.state.board;
+  board[block][call].value = value;
+  board[block][call].access = 2;
 		//console.log(board)
 		this.setState({
 			board: [...board],
@@ -112,35 +152,35 @@ export default class App extends React.Component {
 
   render() {
       const lineArr=[0,1,2];
-    return (
-      <View style={styles.container}>
-        <View style={styles.header}>
-          <Text style={styles.headerText}>Sudoku</Text>
-        </View>
-        <View style={styles.board}>
-        {
-            lineArr.map(ind => (
-                <View key={ind} style={styles.boardLines}>
-                {
-                    this.state.board.slice(ind * 3, (ind * 3) + 3).map((call, mapInd) => (
-                        <Block
+      return (
+          <View style={styles.container}>
+              <View style={styles.header}>
+                <Text style={styles.headerText}>Sudoku</Text>
+              </View>
+              <View style={styles.board}>
+              {
+                lineArr.map(ind => (
+                    <View key={ind} style={styles.boardLines}>
+                    {
+                        this.state.board.slice(ind * 3, (ind * 3) + 3).map((call, mapInd) => (
+                            <Block
                             calls={call}
                             onChangeCall={this.handleChangeCall}
                             index={(ind * 3) + mapInd}
                             key={(ind * 3) + mapInd}
-                        />
+                            />
+                            ))
+                    }
+                    </View>
                     ))
-                }
-                </View>
-            ))
-        }
+            }
+            </View>
+            <View style={styles.footer}>
+                <Button title="New" onPress={() => this.handleGenerateBoard()}></Button>
+                <Button title="Cancel" onPress={() => 5}></Button>
+            </View>
         </View>
-        <View style={styles.footer}>
-          <Button title="New" onPress={() => 5}></Button>
-          <Button title="Cancel" onPress={() => 5}></Button>
-        </View>
-      </View>
-    );
+        );
   }
 }
 
@@ -153,31 +193,31 @@ const styles = StyleSheet.create({
     // backgroundColor: '#aaa',
     //fontSize: 60,
     //textAlign: 'center'
-  },
-  headerText: {
+},
+headerText: {
     textAlign: 'center',
     fontSize: 20,
-  },
-  board: {
+},
+board: {
     height: 375,
     flex: 0,
-  	backgroundColor: '#aaa',
-	flexDirection: 'column',
-  	justifyContent: 'flex-start',
-  },
-  boardLines: {
-    flex: 1,
-  	flexDirection: 'row',
+    backgroundColor: '#aaa',
+    flexDirection: 'column',
     justifyContent: 'flex-start',
-  },
-  footer: {
+},
+boardLines: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+},
+footer: {
 	flexDirection: 'row',
 	justifyContent: 'space-around',
-    },
-  container: {
+},
+container: {
     flex: 1,
     backgroundColor: '#fff',
     //alignItems: 'center',
     justifyContent: 'flex-start',
-  },
+},
 });
---
+++ utils/index.js
@@ -0,0 +1,20 @@
+export default {
+	generateNumberBetween: (min, max) => Math.floor(Math.random()*(max-min+1)+min),
+	numRow2numBlock: row => {
+		//5  row
+	    //4,5,6 block
+	},
+	numRow2numCallInBlock: row => {
+		//5  row
+	    //6,7,8 call
+	},
+	numColumn2numBlock: column => {
+
+	},
+	numColumn2numCallInBlock: column => {
+
+	},
+	checkRow: (row, column) => {
+	    
+	},
+};
